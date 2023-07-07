4c1733c4b1950835cecd236de1ca6d034f8a3b87
Loren Goodwin <18320375+duckdotapk@users.noreply.github.com>
2023-07-06 21:43:26 -0400

    Initial version.

---
+++ src/classes/FritterStaticMiddleware.ts
@@ -0,0 +1,259 @@
+//
+// Imports
+//
+
+import crypto from "node:crypto";
+import fs from "node:fs";
+import path from "node:path";
+import zlib from "node:zlib";
+
+import { FritterMiddlewareFunction } from "@fritter/core";
+import isCompressible from "compressible";
+import mimeTypes from "mime-types";
+
+//
+// Class
+//
+
+export interface FritterStaticMiddlewareFile
+{
+	onDiskFilePath : string;
+
+	md5 : string;
+
+	modifiedDate : Date;
+
+	size : number;
+
+	stats : fs.Stats;
+
+	type : string;
+}
+
+/** Options for constructing a new instance of the static middleware. */
+export interface FritterStaticMiddlewareOptions
+{
+	/** The value of the Cache-Control header. Overrides maxAge. */
+	cacheControlHeader? : string;
+
+	/** One or more directories to serve files from, prioritized from first to last if files exist in multiple directories. */
+	dirs : string[];
+
+	/** Whether to enable gzip compression. */
+	enableGzip? : boolean;
+
+	/** The maximum age of the cache in seconds. */
+	maxAge? : number;
+}
+
+/** A class for constructing middlewares that serve static files. */
+export class FritterStaticMiddleware
+{
+	/** The middleware function. */
+	public readonly execute : FritterMiddlewareFunction;
+
+	/** A cache of file data. */
+	public fileDataCache : { [filePath : string] : FritterStaticMiddlewareFile } = {};
+
+	/** Constructs a new instance of the middleware. */
+	constructor(options : FritterStaticMiddlewareOptions)
+	{
+		//
+		// Default Options
+		//
+
+		options.enableGzip ??= true;
+
+		options.maxAge ??= 0;
+
+		options.cacheControlHeader ??= "public, max-age=" + options.maxAge;
+
+		//
+		// Create Middleware
+		//
+
+		this.execute = async (context, next) =>
+		{
+			//
+			// Check Method
+			//
+
+			if (context.fritterRequest.getHttpMethod() != "GET" && context.fritterRequest.getHttpMethod() != "HEAD")
+			{
+				return await next();
+			}
+
+			//
+			// Get Path
+			//
+
+			let requestedFilePath = path.normalize(decodeURIComponent(context.fritterRequest.getPath()));
+
+			if (path.basename(requestedFilePath) == ".")
+			{
+				return await next();
+			}
+
+			if (requestedFilePath.charAt(0) == path.sep)
+			{
+				requestedFilePath = requestedFilePath.slice(1);
+			}
+
+			//
+			// Get File Data from Cache
+			//
+
+			let file = this.fileDataCache[requestedFilePath];
+
+			//
+			// Load File Data (if not cached)
+			//
+
+			if (file == null)
+			{
+				//
+				// Iterate Directories
+				//
+
+				for (const dir of options.dirs)
+				{
+					//
+					// Build File Path
+					//
+
+					const onDiskFilePath = path.join(dir, requestedFilePath);
+
+					//
+					// Prevent Directory Traversal
+					//
+
+					if (!onDiskFilePath.startsWith(dir))
+					{
+						return await next();
+					}
+
+					//
+					// Get File Stats
+					//
+
+					let stats : fs.Stats;
+
+					try
+					{
+						stats = await fs.promises.stat(onDiskFilePath);
+					}
+					catch (error)
+					{
+						continue;
+					}
+
+					if (!stats.isFile())
+					{
+						continue;
+					}
+
+					//
+					// Create File Data
+					//
+
+					file =
+						{
+							onDiskFilePath,
+
+							md5: crypto.createHash("md5").update(await fs.promises.readFile(onDiskFilePath)).digest("hex"),
+
+							modifiedDate: stats.mtime,
+
+							size: stats.size,
+
+							stats,
+
+							type: mimeTypes.lookup(onDiskFilePath) || "application/octet-stream",
+						};
+
+					this.fileDataCache[requestedFilePath] = file;
+				}
+
+				if (file == null)
+				{
+					return await next();
+				}
+			}
+
+			//
+			// Check On Disk File Modified Date
+			//
+
+			const stats = await fs.promises.stat(file.onDiskFilePath);
+
+			if (stats.mtimeMs != file.stats.mtimeMs)
+			{
+				file.md5 = crypto.createHash("md5").update(await fs.promises.readFile(file.onDiskFilePath)).digest("hex");
+
+				file.modifiedDate = stats.mtime;
+
+				file.size = stats.size;
+
+				file.stats = stats;
+
+				file.type = mimeTypes.lookup(file.onDiskFilePath) || "application/octet-stream";
+			}
+
+			//
+			// Response
+			//
+
+			context.fritterResponse.setStatusCode(200);
+
+			context.fritterResponse.setLastModified(file.modifiedDate);
+
+			context.fritterResponse.setEntityTag(file.md5);
+
+			if (options.enableGzip)
+			{
+				context.fritterResponse.appendVaryHeaderName("Accept-Encoding");
+			}
+
+			if (context.fritterRequest.isFresh())
+			{
+				context.fritterResponse.setStatusCode(304);
+
+				return;
+			}
+
+			context.fritterResponse.setContentType(file.type);
+
+			context.fritterResponse.setContentLength(file.size);
+
+			context.fritterResponse.setHeaderValue("Cache-Control", options.cacheControlHeader as string);
+
+			context.fritterResponse.setHeaderValue("Content-MD5", file.md5);
+
+			if (context.fritterRequest.getHttpMethod() == "HEAD")
+			{
+				return;
+			}
+
+			const readStream = fs.createReadStream(file.onDiskFilePath);
+
+			context.fritterResponse.setBody(readStream);
+
+			const acceptsGzip = context.fritterRequest.getAccepts().encoding("gzip") != null;
+
+			const shouldGzip = options.enableGzip && file.size > 1024 && isCompressible(file.type);
+
+			if (acceptsGzip && shouldGzip)
+			{
+				context.fritterResponse.removeHeaderValue("Content-Length");
+
+				context.fritterResponse.setHeaderValue("Content-Encoding", "gzip");
+
+				context.fritterResponse.setBody(readStream.pipe(zlib.createGzip()));
+			}
+			else
+			{
+				context.fritterResponse.setBody(readStream);
+			}
+		};
+	}
+}
